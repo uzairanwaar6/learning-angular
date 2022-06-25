c40fef9361561b0049988feb12c6038145cb1d66
Uzair <uzairanwaar6@gmail.com>
2022-06-25 17:35:17 +0500

    Some practice

---
+++ practice/src/app/warning/warning.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './warning.component.html',
+  styleUrls: ['./warning.component.css']
+})
+export class WarningComponent implements OnInit {
+
+  message: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
