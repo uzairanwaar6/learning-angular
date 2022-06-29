c3ae852d12f0e00c836838a9a6fc2b635ea9d7b1
Uzair <uzairanwaar6@gmail.com>
2022-06-30 00:30:00 +0500

    Some changes

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { EditComponent } from './recipes/edit/edit.component';
+import { DeleteComponent } from './recipes/delete/delete.component';
+import { DetailComponent } from './recipes/detail/detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    EditComponent,
+    DeleteComponent,
+    DetailComponent,
+    ShoppingListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
